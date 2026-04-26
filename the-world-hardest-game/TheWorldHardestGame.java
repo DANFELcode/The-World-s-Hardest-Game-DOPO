dcdf2780d71b14a6b8dc8833d2d910d810020db5
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-04-26 18:46:06 -0500

    Add files via upload

---
+++ the-world-hardest-game/TheWorldHardestGame.java
@@ -0,0 +1,32 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class TheWorldHardestGame {
+	private ArrayList<Nivel> niveles;
+	private int nivelActual;
+	private int totalMuertes;
+	
+	public TheWorldHardestGame() {
+		this.niveles = new ArrayList<Nivel>();
+		this.totalMuertes = 0;
+	}
+	
+	public void iniciarJuego() {
+		
+	}
+	
+	public void siguienteNivel() {
+		
+	}
+	
+	public void reiniciarNivel() {
+		
+	}
+	
+	public boolean juegoTerminado(){
+		return false;
+	}
+	
+
+}
