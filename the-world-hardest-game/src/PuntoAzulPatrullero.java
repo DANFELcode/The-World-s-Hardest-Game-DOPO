8ae3b273f95fa7d51a6ac1c688737c151237f68b
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-04-28 17:20:51 -0500

    Add route points and current index to PuntoAzulPatrullero
    Added fields for route points and current index.

--- the-world-hardest-game/src/PuntoAzulPatrullero.java
+++ the-world-hardest-game/src/PuntoAzulPatrullero.java
@@ -1,8 +1,12 @@
 package domain;
 
 import java.awt.Rectangle;
+import java.util.List;
 
 public class PuntoAzulPatrullero extends Enemigo {
+	private List<int[]> puntoRuta;
+	private int indiceActual;
+	
     public PuntoAzulPatrullero(int x, int y) {
         super(x, y, 1.0, "azul");
     }
