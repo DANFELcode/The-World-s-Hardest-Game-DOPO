4c6faba128b97efb74b60dac47dfe713519d1210
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-04-28 17:22:04 -0500

    Remove unused method and clean up code

--- the-world-hardest-game/src/ZonaSeguraIntermedia.java
+++ the-world-hardest-game/src/ZonaSeguraIntermedia.java
@@ -8,9 +8,6 @@ public class ZonaSeguraIntermedia extends Objetivo {
     }
 
     @Override
-    public void alContacto(Jugador jugador) {}
-
-    @Override
     public void alColisionar(Jugador jugador) {}
 
 	@Override
