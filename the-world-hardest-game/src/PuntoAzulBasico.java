d9ea3e7f63838525a6013f320742b5f3bfea5129
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-04-28 17:21:13 -0500

    Add movement attributes to PuntoAzulBasico class
    Added horizontal movement and direction attributes to PuntoAzulBasico.

--- the-world-hardest-game/src/PuntoAzulBasico.java
+++ the-world-hardest-game/src/PuntoAzulBasico.java
@@ -3,6 +3,9 @@ package domain;
 import java.awt.Rectangle;
 
 public class PuntoAzulBasico extends Enemigo {
+	private boolean horizontal;  // true = mov. horizontal, false = vertical
+	private int direccion; 
+	
     public PuntoAzulBasico(int x, int y) {
         super(x, y, 1.0, "azul");
     }
