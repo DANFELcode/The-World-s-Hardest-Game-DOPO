3318f61571424f0f79529aa7a46d568d7dc60c96
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-04-28 19:20:13 -0500

    Change coordinates from int to double in Objetivo

--- the-world-hardest-game/src/Objetivo.java
+++ the-world-hardest-game/src/Objetivo.java
@@ -1,21 +1,20 @@
 package domain;
 
 public abstract class Objetivo implements Colisionable {
-    private int x;
-    private int y;
+    private double x;
+    private double y;
     private boolean obtenido;
 
-    public Objetivo(int x, int y) {
+    public Objetivo(double x, double y) {
     	this.x = x;
     	this.y = y;
     	this.obtenido = false;
-    } 
+    }
 
     public boolean isObtenido() {
     	return obtenido;
     }
-    
-    
-    public int getX() {return 0;}
-    public int getY() {return 0;}    
+
+    public double getX() { return x; }
+    public double getY() { return y; }
 }
