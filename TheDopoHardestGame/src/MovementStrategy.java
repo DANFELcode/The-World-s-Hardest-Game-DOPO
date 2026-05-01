16e013a37d26869828071a2ee970b769792e4077
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-05-01 18:35:54 -0500

    Add files via upload

---
+++ TheDopoHardestGame/src/MovementStrategy.java
@@ -0,0 +1,14 @@
+package domain;
+
+/**
+ * Defines the movement behavior of an enemy. <br>
+ */
+public interface MovementStrategy {
+
+    /**
+     * Moves the enemy according to the strategy.
+     * @param enemy the enemy to move
+     * @param map the current level map
+     */
+    void move(Enemigo enemy, Mapa map);
+}
