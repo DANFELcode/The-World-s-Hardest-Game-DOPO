79d202f0871d60f3843e795fa05fefa4e480694a
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-05-02 18:53:47 -0500

    Revise InitialZone class documentation
    Updated class documentation and added parameter descriptions.

--- TheDopoHardestGame/src/InitialZone.java
+++ TheDopoHardestGame/src/InitialZone.java
@@ -1,10 +1,19 @@
 package domain;
 
 /**
- * Área desde la cual parte el jugador al inicio del juego o al morir[cite: 1].
+ * Represents the starting zone of the level. Players spawn here at the start or after dying. <br>
+ * <b>(x, y, width, height)</b> <br>
+ * <b>Inv:</b> width > 0 and height > 0
  */
 public class InitialZone extends Zone {
 
+    /**
+     * Creates an initial zone at the given position and size.
+     * @param x horizontal position
+     * @param y vertical position
+     * @param width zone width, must be greater than 0
+     * @param height zone height, must be greater than 0
+     */
     public InitialZone(double x, double y, double width, double height) {
         super(x, y, width, height);
     }
