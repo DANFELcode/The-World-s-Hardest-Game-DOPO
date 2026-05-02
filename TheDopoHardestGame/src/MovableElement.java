e18c2b75f42b5616653466087f24ca048b0f9a01
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-05-02 18:55:44 -0500

    Refactor MovableElement class documentation and getters
    Updated class documentation and simplified getter methods.

--- TheDopoHardestGame/src/MovableElement.java
+++ TheDopoHardestGame/src/MovableElement.java
@@ -1,7 +1,9 @@
 package domain;
 
 /**
- * Clase base abstracta para todos los elementos que pueden moverse en el juego.
+ * Represents a movable element in the game. <br>
+ * <b>(x, y, width, height, speed)</b> <br>
+ * <b>Inv:</b> speed > 0 and width > 0 and height > 0
  */
 public abstract class MovableElement {
     protected double x;
@@ -10,6 +12,14 @@ public abstract class MovableElement {
     protected double height;
     protected double speed;
 
+    /**
+     * Creates a movable element with an initial position, size and speed.
+     * @param x initial horizontal position
+     * @param y initial vertical position
+     * @param width element width, must be greater than 0
+     * @param height element height, must be greater than 0
+     * @param speed movement speed, must be greater than 0
+     */
     public MovableElement(double x, double y, double width, double height, double speed) {
         this.x = x;
         this.y = y;
@@ -19,33 +29,18 @@ public abstract class MovableElement {
     }
 
     /**
-     * Actualiza las coordenadas del elemento en el mapa.
-     * 
-     * @param newX la nueva coordenada X
-     * @param newY la nueva coordenada Y
+     * Updates the position of the element.
+     * @param newX new horizontal position
+     * @param newY new vertical position
      */
     public void setPosition(double newX, double newY) {
         this.x = newX;
         this.y = newY;
     }
 
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public double getSpeed() {
-        return speed;
-    }
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
+    public double getSpeed() { return speed; }
 }
