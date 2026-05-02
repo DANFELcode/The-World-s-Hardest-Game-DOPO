cdb11f7ce17b73398a97adebeae1628906545e2d
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-05-02 18:58:29 -0500

    Enhance documentation for RedPlayer class
    Updated class documentation and added parameter descriptions.

--- TheDopoHardestGame/src/RedPlayer.java
+++ TheDopoHardestGame/src/RedPlayer.java
@@ -1,14 +1,26 @@
 package domain;
 
 /**
- * Representa al jugador estándar con velocidad y tamaño normales
+ * Represents the standard player with normal speed and size. <br>
+ * <b>(name, x, y, width=20.0, height=20.0, speed=1.0)</b> <br>
+ * <b>Inv:</b> speed > 0 and width > 0 and height > 0
  */
 public class RedPlayer extends Player {
 
+    /**
+     * Creates a red player at the given position with default size (20x20) and speed (1.0).
+     * @param name player name
+     * @param x initial horizontal position
+     * @param y initial vertical position
+     */
     public RedPlayer(String name, double x, double y) {
         super(name, x, y, 20.0, 20.0, 1.0);
     }
 
+    /**
+     * Changes the player's skin stats based on the collected skin coin color.
+     * @param newSkin the color of the skin coin collected
+     */
     @Override
     public void changeSkin(String newSkin) {
         if (newSkin.equals("Blue")) {
