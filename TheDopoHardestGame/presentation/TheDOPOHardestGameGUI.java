94662cc1220e2c0a6c21b442dc40de057088899f
Juan Munar <juandamuch13@gmail.com>
2026-05-03 18:26:55 -0500

    Add files via upload

---
+++ TheDopoHardestGame/presentation/TheDOPOHardestGameGUI.java
@@ -0,0 +1,57 @@
+package presentation;
+
+import domain.TheDOPOHardestGame;
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+
+
+public class TheDOPOHardestGameGUI extends JFrame {
+	
+	private TheDOPOHardestGame juego;
+    private JMenuBar menuBar;
+    private JMenu opciones, archivo;
+    private JMenuItem nuevaPartida, pausar, salir, reiniciar;
+    private JMenuItem guardarPartida, abrirPartida;
+    
+    private JPanel panel;
+	private CardLayout cardLayout;
+	
+	
+	//Panel inicio
+    private JPanel panelInicio; 
+    private JLabel titulo;
+    private JButton playGame;
+    private JButton settings;
+    
+	//Panel Explicacion
+    private JPanel panelExp; 
+    private JLabel descripcion;
+    private JButton playGame2;
+    private JButton backInicio;
+
+    
+    //Panel Juego
+    private JPanel panelJuego;
+    private JPanel tablero;
+    private JButton menu;
+    private JLabel niveles;
+    private JLabel muertes;
+    private JLabel tiempo;
+    
+    
+    
+
+
+    
+
+    
+
+
+
+   
+    
+
+	
+	
+}
