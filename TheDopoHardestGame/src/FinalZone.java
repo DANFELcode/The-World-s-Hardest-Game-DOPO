57f8dfbbd002f1bc9ab2351843d5d7df052a2b98
Juan Munar <juandamuch13@gmail.com>
2026-05-03 17:18:08 -0500

    Add files via upload

---
+++ TheDopoHardestGame/src/FinalZone.java
@@ -0,0 +1,12 @@
+package domain;
+
+/**
+ * Área a la que debe llegar el jugador después de recolectar todas las
+ * monedas
+ */
+public class FinalZone extends Zone {
+
+    public FinalZone(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
+}
