64f0b6718631a7d26beb69f6a3d27ac64f10455e
DANFELcode <149917870+DANFELcode@users.noreply.github.com>
2026-05-14 16:41:25 -0500

    Delete TheDopoHardestGame/src directory

--- TheDopoHardestGame/src/GameException.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public class GameException extends Exception {
-
-	public GameException(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- TheDopoHardestGame/src/Interactable.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-import java.awt.geom.Rectangle2D;
-
-public interface Interactable {
-    void onPlayerContact(Player player, Level level);
-    Rectangle2D getAreaColision();
-    default boolean shouldRemove() { return false; }
-}
