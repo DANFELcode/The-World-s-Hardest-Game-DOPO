7a235940e6604f9c1228c86276c9b5b3c51af641
Juan Munar <juandamuch13@gmail.com>
2026-05-25 13:34:24 -0500

    Delete TheDopoHardestGame/domain directory

--- TheDopoHardestGame/domain/Direction.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-/**
- * Axis along which a LinearMovement enemy travels. <br>
- * HORIZONTAL: moves left/right. VERTICAL: moves up/down.
- */
-public enum Direction {
-    HORIZONTAL, VERTICAL
-}
